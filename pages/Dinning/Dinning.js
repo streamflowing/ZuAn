a70e715d61ead503dcf3a596e84f02cb038009d1
streamflowing <2991180094@qq.com>
2020-07-30 16:11:42 +0800

    Initial Commit

---
+++ pages/Dinning/Dinning.js
@@ -0,0 +1,92 @@
+// pages/Dinning/Dinning.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    DishInfo:  [
+      {
+      dishimage:"/asset/image/Din/dish0.png",
+      dishname:"章汉良",
+      dishleft:1,
+      dishprice:9.9},
+      {
+        dishimage:"/asset/image/Din/dish1.png",
+        dishname:"锅包肉",
+        dishleft:20,
+        dishprice:7.0},
+        {
+          dishimage:"/asset/image/Din/dish2.png",
+          dishname:"狮子头",
+          dishleft:40,
+          dishprice:5.0},
+          {
+            dishimage:"/asset/image/Din/dish3.png",
+            dishname:"毛血旺",
+            dishleft:60,
+            dishprice:4.5},
+      
+
+    ]
+    
+
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
